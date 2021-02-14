89422559bd507368e330228eede8c203ffdc644b
dipu <dipu.j247@gmail.com>
2021-02-14 10:44:09 +0600

    django food application

---
+++ res_info/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+# class RestaurentInfo(models.Model):
+#     res_name = models.CharField(max_length=200)
+#     res_address = models.TextField()
+#
+#     class Meta:
+#         ordering = ['res_name']
+#
+#     def __str__(self):
+#         return self.res_name
